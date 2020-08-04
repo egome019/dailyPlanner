db6218ff0b361ab5192781f37d516f600660c305
egomez <egomezfax19@gmail.com>
2020-08-04 09:38:45 -0700

    fixed the color of the textarea so it went with the time

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
 $(document).ready(function() {
+
+    // this is all for the time to show on the header
     var currentDay = $("#currentDay");
     var format = 'dddd MMM Do';
     var result = moment().format(format);
 
     currentDay.text(result);
-
+// this is the beginning of the color changing based on the time
     var timeBlock = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-
 
     function timeBlockColor(){
         var currentTime = moment().format('ha')
         for (var index = 0; index < timeBlock.length; index++) {
-            if (parseInt(timeBlock[index]) > currentTime) {
+            if ((timeBlock[index]) < currentTime) {
                 $("#" + timeBlock[index]).addClass("future")
             }
-            else if (parseInt(timeBlock[index]) < currentTime) {
+            else if ((timeBlock[index]) > currentTime) {
                 $("#" + timeBlock[index]).addClass("past")
             }
-            else if (parseInt(timeBlock[index]) == currentTime) {
+            else if ((timeBlock[index]) == currentTime) {
                 $("#" + timeBlock[index]).addClass("present")
             }
         }
     }
 
+// this is where the plans for the timeblock get saved to the time block even when refreshing the page.
     var storedAppt = [];
+
 
     function plans() {
         var storedInputs = JSON.parse(localStorage.getItem("times"));
         if (storedInputs !== null) {
             storedAppt = storedInputs;
-            storedAppts();
             timeBlockColor();
+            storedAppts();
         }else{
             timeBlockColor();
         }
     }
-
+    plans();
     function storedAppts() {
         for (var index = 0; index < storedAppt.length; index++) {
             var storedEvent = storedAppt[index].event;
@@ -49,27 +52,18 @@ $(document).ready(function() {
         }
     }
 
-
+// this the click function that stores the plans in each time block when the save button is clicked
 
     $(".saveBtn").on("click", function(e){
         e.preventDefault();
 
         var hourBlock = $(this).prev().attr("id");
         var appt = $(this).prev().val().trim();
-
-        console.log(hourBlock)
-        console.log(appt)
-
         var times = {
                 time: hourBlock,
                 event: appt,
             };
-
-        
         storedAppt.push(times);
-        localStorage.setItem("times", JSON.stringify(storedAppt));
-        
+        localStorage.setItem("times", JSON.stringify(storedAppt));  
     })
-    
-
 })
